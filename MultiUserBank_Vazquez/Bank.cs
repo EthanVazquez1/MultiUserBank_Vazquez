2ca30e44db422cde466ffbfe4e7708ab8e05016a
Ethan Vazquez <Ethan.Vazquez050@gmail.com>
2024-05-15 13:44:10 -0700

    Add project files.

---
+++ MultiUserBank_Vazquez/Bank.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiUserBank_Vazquez
+{
+    
+    internal class Bank
+    {
+        int[] userBalance = { 1250, 2500, 3000, 1001 };
+        const int startingBalance = 10000;
+        private int balance = startingBalance;
+
+        public int GetuserBalance(int userID)
+        {
+            return userBalance[userID];
+        }
+
+        public int GetBalance()
+        {
+            return balance;
+        }
+
+        public int add(int amount, int userID)
+        {
+            balance += amount;
+            return userBalance[userID] += amount;
+        }
+        public int subtract(int amount, int userID)
+        {
+            if (userBalance[userID] < amount || amount >= balance)
+            {
+                return userBalance[userID];
+            }
+            if (amount > 500)
+            {
+                amount = 500;
+            }
+            if (userBalance[userID] - amount <= 0)
+            {
+                amount = balance;
+            }
+            userBalance[userID] -= amount;
+            balance -= amount;
+            return balance;
+        }
+    }
+}
---
+++ MultiUserBank_Vazquez/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Numerics;
+
+namespace MultiUserBank_Vazquez
+{
+    internal class Program
+    {
+            public static void Main(string[] args)
+            {
+            Bank Twinstar = new Bank();
+            bool bankActive = true;
+            
+            string[] myUsers = { "jlennon", "pmccartney", "gharrison", "rstarr" };
+            string[] myPass = { "johnny", "pauly", "georgy", "ringoy" };
+            string Login = "";
+            string passLogin = "";
+            int userID = -1;
+            bool isLoginValid = false;
+
+            void logIn()
+            {
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("Welcome to TwinStar. Please log in");
+                    Console.WriteLine("The bank balance is " + Twinstar.GetBalance().ToString("C"));
+                    Console.WriteLine("Type in your username");
+                    Login = Console.ReadLine();
+
+
+                    Console.WriteLine("Please enter your password");
+                    passLogin = Console.ReadLine();
+                    for (int i = 0; i < myUsers.Length; i++)
+                    {
+                        if (Login == myUsers[i])
+                        {
+                            if (passLogin == myPass[i])
+                            {
+                                isLoginValid = true;
+                                userID = i;
+                            }
+                        }
+                    }
+                } while (!isLoginValid);
+            }
+            logIn();
+                do
+                {
+                    Console.WriteLine("Hello " + Login);
+                    //Console.WriteLine("The bank balance is " + Twinstar.GetBalance().ToString("C"));
+                    Console.WriteLine("What would you like to do?");
+                    Console.WriteLine("Your options are: 1.Check balance  2.Deposit  3.Withdrawing  4.Log out");
+                    string answer = Console.ReadLine();
+
+
+                    if (answer == "1")
+                    {
+                    Console.WriteLine("Your current balance is " + Twinstar.GetuserBalance(userID).ToString("C"));
+                    }
+
+                    else if (answer == "2")
+                    {
+                    Console.WriteLine("How much would you like to deposit?");
+                    string addinput = Console.ReadLine();
+                    int addamount = int.Parse(addinput);
+                    Twinstar.add(addamount, userID);
+                    }
+
+                    else if (answer == "3")
+                    {
+                    Console.WriteLine("How much would you like to withdrawl?");
+                    string subtractinput = Console.ReadLine();
+                    int subtractamount = int.Parse(subtractinput);
+                    Twinstar.subtract(subtractamount, userID);
+                    }
+
+                    else if (answer == "4")
+                    {
+                         logIn();
+                    }
+                } while (bankActive);
+            }
+    }
+}
